27dbaaf57a63e5db06df1712a41f1a6818946ce1
Prem <prem@wiseai.tech>
2021-08-15 21:05:02 +0800

    Updates

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-7d7b24b52a49e7df0f3b.js"
   },
   {
-    "url": "app-d0070d178a653602aad7.js"
+    "url": "app-0bcea669d76c2ba03ec0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "82e9dedde89c136f3d28adff1980e43d"
+    "revision": "11d8916a87b21db7901704dfe6e8585a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-30d94e0b349286bcc42f.js"
@@ -139,7 +139,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-d0070d178a653602aad7.js`))) {
+  if (!resources || !(await caches.match(`/app-0bcea669d76c2ba03ec0.js`))) {
     return await fetch(event.request)
   }
 
