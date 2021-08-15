de98fffd8b3d63653c3e29adad9bdf2676fd30cd
Prem <prem@wiseai.tech>
2021-08-15 21:49:38 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-071be1105c91c7053c80.js"
+    "url": "webpack-runtime-5f22e3b375641e0abd21.js"
   },
   {
     "url": "framework-7d7b24b52a49e7df0f3b.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3404d757dddd8e7cff14f17d72f4348a"
+    "revision": "e0df3df7c3bafd09a26ec6d2387d3f83"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-30d94e0b349286bcc42f.js"
